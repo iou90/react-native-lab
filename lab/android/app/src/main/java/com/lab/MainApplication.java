f949eaa82342d7abfa4fb8efecf6de890d2f6fc3
iou90 <iou90@outlook.com>
2017-01-09 20:24:19 +0800

    remove react-native-fs; test autoheightwebview on android

--- lab/android/app/src/main/java/com/lab/MainApplication.java
+++ lab/android/app/src/main/java/com/lab/MainApplication.java
@@ -4,8 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.dscj.autoheightwebview.AutoHeightWebViewPackage;
 import com.brentvatne.react.ReactVideoPackage;
-import com.rnfs.RNFSPackage;
 import com.dskj.dscj.aliyunoss.aliyunossPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -27,8 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AutoHeightWebViewPackage(),
             new ReactVideoPackage(),
-            new RNFSPackage(),
             new aliyunossPackage()
       );
     }
--- lab/index.android.js
+++ lab/index.android.js
@@ -7,26 +7,22 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  ScrollView,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
+import AutoHeightWebView from 'react-native-autoheightwebview';
+
 export default class lab extends Component {
   render() {
+    const script = `document.body.style.background = 'lightyellow';`;
+    const html = `<p>1. &nbsp;课程内容：<span style=\"color: rgb(51, 51, 51); font-size: 14px; line-height: 24px; white-space: normal;\">普通话、</span><span style=\"color: rgb(51, 51, 51); font-size: 14px; line-height: 24px; white-space: normal;\">综合素质系统班（中学）、教育知识与能力系统班（中学）、冲刺班、面试实战班</span>。<span style=\"font-weight: 700; color: rgb(51, 51, 51); font-size: 14px; line-height: 24px;\">（不含学科知识与能力）</span></p><div><p>2. &nbsp;课程特色：全程保驾护航，省心省力，包含所有班次的全套服务，理论课程无限次免费听，并且配备私人助教辅导学习。</p><p>3. &nbsp;课程时间：笔试课程9月19日-10月28日；笔试冲刺班开课时间为考前一周，面试实战班开课时间为考前一个月。</p></div><div><p>4. &nbsp;购课方式：点师成金APP内购课，购课后加群<span style=\"font-size:16px;font-family: 宋体\">294085100</span>联系助教。</p></div><div><p>5. &nbsp;适用人群：备考2016中<span style=\"color: rgb(51, 51, 51); font-size: 14px; line-height: 24px;\">小</span>学教师资格考试学员，不适用幼儿园学员。</p><p><br/></p></div>`;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <AutoHeightWebView
+        customScript={script}
+        html={html} />
     );
   }
 }
@@ -37,17 +33,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('lab', () => lab);
